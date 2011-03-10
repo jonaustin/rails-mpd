36b97aa45af12db6a721cdb7b4a4a2a93a0080a1
Jon Austin <jonaustin@jonaustin.org>
2011-03-09 20:54:49 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nyx2::Application.config.secret_token = '437a10db0574b5a80d014df89a61433676f67d10d483689f1d5a2db6a950cad4122671b92a57f558699cd119a0999ae3edb604a037bc6f12fcf9c95ac2724085'
