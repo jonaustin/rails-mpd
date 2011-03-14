c26f353c7e45bd77f55cf605230b65e6f4eab77c
Jon Austin <jonaustin@jonaustin.org>
2011-03-13 18:45:35 -0700

    Move Constants above initialize! (cucumber was choking, and probably the right way to do it anyway)

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Stratum::Application.initialize!
-
 # App Constants (will eventually move into user web prefs)
 PLAYER_BACKEND = 'MPD'
 #MUSIC_DIR     = '/media/MORGOTH/musix'
@@ -11,3 +8,7 @@ MUSIC_DIR     = '/home/jon/.mpd_local/music' # for testing
 PLAYLIST_DIR  = '/home/jon/.mpd_local/playlists'
 MPD_HOST      = 'localhost'
 MPD_PORT      = 6600
+
+# Initialize the rails application
+Stratum::Application.initialize!
+
