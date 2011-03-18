c7907144978f30bdf4d2f75ea57189a8f70389d3
Jon Austin <jonaustin@jonaustin.org>
2011-03-18 13:55:56 -0700

    Add final initial specs

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -27,7 +27,7 @@ describe Player do
       end
 
       context "to play/pause/stop playback" do
-        it "should play if stopped when receiving play() message" do
+        it "should play when receiving play() message" do
           @player.stop if @player.playing?
           @player.play(0)
           @player.playing?.should be_true
@@ -42,9 +42,24 @@ describe Player do
           @player.pause_play if @player.playing?
           @player.paused?.should be_true
         end
+
+        it "should stop when receiving stop() message" do
+          @player.play(0) if @player.stopped?
+          @player.stop
+          @player.playing?.should be_false
+        end
+
+        context "for repeat/random state" do
+          it "should switch repeat state when receiving repeat() message" do
+            initial_repeat = @player.repeat?
+            @player.repeat
+            new_repeat = @player.repeat?
+            new_repeat.should_not eq(initial_repeat)
+          end
+        end
       end
 
-      context "for next and previous" do
+      context "for next or previous" do
         before(:each) do
           @start_pos = 1
           @player.play(@start_pos)
@@ -65,6 +80,8 @@ describe Player do
           pos.should eq(@start_pos+1) # test position after switching to previous
         end
       end
+
+
     end
   end
 end
