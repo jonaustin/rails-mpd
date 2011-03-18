59a68bd6a53247ca5887ac9611d217a555a97162
Jon Austin <jonaustin@jonaustin.org>
2011-03-18 01:43:43 -0700

    moved all PlayerAbstract (currently only MPDAbstract) logic from app..ctrl spec to /lib/player spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,80 +1,19 @@
 require 'spec_helper'
-require 'net/telnet'
-# Only going to deal with MPD as a backend for now, so use net/telnet interface to verify.
 
 describe ApplicationController do
-  describe "modify play state" do
-    it "should go to previous track when H" do
-      pending
-      @mpd.previous
-    end
-
-    it "self.next" do
-      pending
-      @mpd.next
-    end
-
-
-    it "self.pause_play" do
-      pending
-      if @mpd.stopped?
-        @mpd.play 0
-      else
-        @mpd.pause = (@mpd.paused? ? false : true)
+  context "MPD Abstract class" do
+    describe "modify play state" do
+      before(:all) do
+        require 'net/telnet'
       end
-    end
 
-
-    it "self.stop" do
-      pending
-      @mpd.stop
-    end
-
-
-    it "self.repeat" do
-      pending
-      @mpd.repeat = (@mpd.repeat? ? false : true)
-    end
-
-    it "self.playlist_move_up(pos)" do
-      pending
-      @mpd.move pos, pos-1
-    end
-
-    it "self.playlist_move_down(pos)" do
-      pending
-      @mpd.move pos, pos+1
-    end
-
-    it "self.playlist_remove(pos)" do
-      pending
-      @mpd.delete pos
-    end
-
-    ### State
-    it "cur_track" do
-      pending
-      @mpd.current_song
-    end
-
-    it "cur_playlist" do
-      pending
-      @mpd.playlist
-    end
-
-    it "playing?" do
-      pending
-      @mpd.playing?
-    end
-
-    it "stopped?" do
-      pending
-      @mpd.stopped
-    end
-
-    it "repeat?" do
-      pending
-      @mpd.repeat?
+      it "should return a @player that is r valid instance of PLAYER_BACKEND" do
+        pending
+        appc = ApplicationController.new
+        @player = appc.get_player
+        @player.should_be instance_of(MPD)
+      end
     end
   end
 end
+
---
+++ spec/lib/player_spec.rb
@@ -0,0 +1,106 @@
+require 'spec_helper'
+
+describe Player do
+  context "MPD Player" do
+    describe "modify play state" do
+      before(:all) do
+        require 'net/telnet'
+      end
+      before(:each) do
+        @player = Player::MPDAbstract.new # change to let?
+      end
+
+      it "should connect when receiving connect() message" do
+        @player.disconnect if @player.connected?
+        @player.connect
+        @player.connected?.should be_true
+        @player.disconnect
+      end
+
+      it "should play if stopped when receiving play() message" do
+        @player.stop if @player.playing?
+        @player.play(0)
+        @player.playing?.should be_true
+      end
+
+      it "should play if paused when receiving pause_play() message" do
+        @player.pause if @player.playing?
+        @player.paused?.should be_true
+      end
+
+      it "should pause if playing when receiving pause_play() message" do
+        @player.pause_play if @player.playing?
+        @player.paused?.should be_true
+      end
+
+      it "should go to previous track when receiving previous() message" do
+        # refactor..
+        start_pos = 1
+        @player.play(start_pos)
+        @player.playing?.should be_true
+        pos = @player.playlist_pos
+        pos.should eq(1) # test position after playing at specific pos
+        @player.previous
+        pos = @player.playlist_pos
+        pos.should eq(start_pos-1) # test position after switching to previous
+      end
+
+      it "self.next" do
+        pending
+        @player.next
+      end
+
+      it "self.stop" do
+        pending
+        @player.stop
+      end
+
+
+      it "self.repeat" do
+        pending
+        @player.repeat = (@player.repeat? ? false : true)
+      end
+
+      it "self.playlist_move_up(pos)" do
+        pending
+        @player.move pos, pos-1
+      end
+
+      it "self.playlist_move_down(pos)" do
+        pending
+        @player.move pos, pos+1
+      end
+
+      it "self.playlist_remove(pos)" do
+        pending
+        @player.delete pos
+      end
+
+      ### State
+      it "cur_track" do
+        pending
+        @player.current_song
+      end
+
+      it "cur_playlist" do
+        pending
+        @player.playlist
+      end
+
+      it "playing?" do
+        pending
+        @player.playing?
+      end
+
+      it "stopped?" do
+        pending
+        @player.stopped
+      end
+
+      it "repeat?" do
+        pending
+        @player.repeat?
+      end
+    end
+  end
+end
