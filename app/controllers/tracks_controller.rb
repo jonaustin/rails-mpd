33d4956ab90c19ae0e663363a58a4d8ef0ab302a
Jon Austin <jonaustin@jonaustin.org>
2011-06-12 14:17:20 -0700

    adding ability to sync mpd tracks to db

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,5 +1,25 @@
-class TrackController < ApplicationController
+class TracksController < ApplicationController
   def sync_mpd
+    mpd = @player.mpd
+    mpd.connect
+    tracks = mpd.songs
+    tracks.each do |t|
+      artist = Artist.find_or_create_by_name(t.artist)
+      album  = Album.find_or_create_by_name(t.album) #FIXME: find by album/artist
+      Track.create(:name => t.title, :number => t.track.split('/')[0], :duration => t.time, :filename => t.file)
+    end
+    message = "MPD synced:  #{Track.count} tracks"
+
+    respond_to do |format|
+      format.html do
+        if request.xhr?
+          render :js => message, :layout => false, :status => 200
+        else
+          flash[:notice] = message
+          redirect_to "/current"
+        end
+      end
+    end
   end
 
 end
