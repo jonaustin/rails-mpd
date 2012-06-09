dafae6e87f437fe345d0e90983b9e54e0be7f7e9
Jon Austin <jon.i.austin@gmail.com>
2012-06-09 12:47:27 -0700

    refactor

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,5 +1,5 @@
 class PlayerController < ApplicationController
-  #respond_to :js
+  respond_to :js
 
   # make all these into one method
   def controls
@@ -10,76 +10,30 @@ class PlayerController < ApplicationController
 
   def play
     @player.play params['pos']
-    respond_to do |format|
-      format.html do
-        if request.xhr?
-          render :partial => "_partials/songinfo", :locals => { :cur_song => @player.cur_song }, :layout => false, :status => 200
-        else
-          redirect_to  '/current'
-        end
-      end
-    end
   end
 
 
   def previous
     @player.previous
-    respond_to do |format|
-      format.html do
-        if request.xhr?
-          render :partial => "_partials/songinfo", :locals => { :cur_song => @player.cur_song }, :layout => false, :status => 200
-        else
-          redirect_to  '/current'
-        end
-      end
-    end
   end
 
   def next
     @player.next
-    respond_to do |format|
-      format.html do
-        if request.xhr?
-          render :partial => "_partials/songinfo", :locals => { :cur_song => @player.cur_song }, :layout => false, :status => 200
-        else
-          redirect_to  '/current'
-        end
-      end
-    end
   end
 
 
   def pause_play
     @player.pause_play
-    respond_to do |format|
-      format.html do
-        if request.xhr?
-          render :partial => "_partials/songinfo", :locals => { :cur_song => @player.cur_song }, :layout => false, :status => 200
-        else
-          redirect_to  '/current'
-        end
-      end
-    end
   end
 
 
   def stop
     @player.stop
-    respond_to do |format|
-      format.html do
-        if request.xhr?
-          render :partial => "_partials/songinfo", :locals => { :cur_song => @player.cur_song }, :layout => false, :status => 200
-        else
-          redirect_to  '/current'
-        end
-      end
-    end
   end
 
 
   def repeat
     @player.repeat = (@player.repeat? ? false : true)
-    redirect_to  '/current'
   end
 
   ### Playlist Actions
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -10,17 +10,16 @@ class TracksController < ApplicationController
       artist = Artist.find_or_create_by_name(t.artist)
       album  = Album.find_or_create_by_name(t.album) #FIXME: find by album/artist
       number = t.track.split('/')[0] unless t.track.nil?
-      Track.create(:name => t.title, :number => number, :duration => t.time, :filename => t.file, :artist_id => artist.id, :album_id => album.id)
+      Track.create(name: t.title, number: number, duration: t.time, filename: t.file, artist_id: artist.id, album_id: album.id)
     end
     message = "MPD synced:  #{Track.count} tracks"
 
     respond_to do |format|
       format.html do
         if request.xhr?
-          render :js => message, :layout => false, :status => 200
+          render js: message, layout: false, status: 200
         else
-          flash[:notice] = message
-          redirect_to "/current"
+          redirect_to "/current", notice: message
         end
       end
     end
